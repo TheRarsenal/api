cf674be94b33a10ba214ca67a2ce057641eb4667
TheRarsenal <apscual119@gmail.com>
2024-04-16 11:17:24 -0600

    modificacion para crear usuario administrador

--- routes/api.php
+++ routes/api.php
@@ -7,9 +7,10 @@ use Illuminate\Support\Facades\Route;
 
 route::post('login',[AuthController::class, 'login']);
 route::post('conf',[AuthController::class, 'conf']);
+route::post('register',[AuthController::class, 'register']);
 
 Route::group(['middleware' => ['auth:sanctum']], function(){
-    route::post('register',[AuthController::class, 'register']);
+    
     route::get('user-profile',[AuthController::class, 'userProfile']);
     route::post('logout',[AuthController::class, 'logout']);
 });
